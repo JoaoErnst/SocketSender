458d7ccba5456ec30ec50206f3f9fb0b247eaa45
João Ernst <74716479+JoaoErnst@users.noreply.github.com>
2023-12-03 16:24:44 -0300

    Add files via upload

---
+++ socketSender.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import socket
+
+print("Interagindo com o Servidor FTP")
+
+ip = input("Digite o IP: ")
+porta = 21
+
+# Solicitar entrada do usuário e senha
+usuario = input("Digite o nome de usuário: ")
+senha = input("Digite a senha: ")
+
+meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+meusocket.connect((ip, porta))
+banner = meusocket.recv(1024)
+print(banner.decode('utf-8'))
+
+# Enviar nome de usuário
+meusocket.send(f"USER {usuario}\r\n".encode('utf-8'))
+banner = meusocket.recv(1024)
+print(banner.decode('utf-8'))
+
+# Enviar senha
+meusocket.send(f"PASS {senha}\r\n".encode('utf-8'))
+banner = meusocket.recv(1024)
+print(banner.decode('utf-8'))
+
+meusocket.close()
+ 
